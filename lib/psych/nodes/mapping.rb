742ee7d2fac9d97beba9aaafca67d8c4879632ba
Aaron Patterson <aaron.patterson@gmail.com>
2009-09-27 20:47:23 -0700

    adding accessors to the mapping

--- lib/psych/nodes/mapping.rb
+++ lib/psych/nodes/mapping.rb
@@ -3,6 +3,18 @@ module Psych
     ###
     # This class represents a {YAML Mapping}[http://yaml.org/spec/1.1/#mapping].
     class Mapping < Psych::Nodes::Node
+      # The optional anchor for this mapping
+      attr_accessor :anchor
+
+      # The optional tag for this mapping
+      attr_accessor :tag
+
+      # Is this an implicit mapping?
+      attr_accessor :implicit
+
+      # The style of this mapping
+      attr_accessor :style
+
       def initialize anchor, tag, implicit, style
         super()
         @anchor   = anchor
