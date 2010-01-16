7e67046a46ebaef74b701dd19209d3854c5ecc2b
Aaron Patterson <aaron.patterson@gmail.com>
2010-01-15 20:43:57 -0800

    formatting

--- lib/psych/visitors/visitor.rb
+++ lib/psych/visitors/visitor.rb
@@ -3,18 +3,12 @@ module Psych
     class Visitor
       def accept target
         case target
-        when Psych::Nodes::Scalar
-          visit_Psych_Nodes_Scalar target
-        when Psych::Nodes::Mapping
-          visit_Psych_Nodes_Mapping target
-        when Psych::Nodes::Sequence
-          visit_Psych_Nodes_Sequence target
-        when Psych::Nodes::Document
-          visit_Psych_Nodes_Document target
-        when Psych::Nodes::Stream
-          visit_Psych_Nodes_Stream target
-        when Psych::Nodes::Alias
-          visit_Psych_Nodes_Alias target
+        when Psych::Nodes::Scalar   then visit_Psych_Nodes_Scalar target
+        when Psych::Nodes::Mapping  then visit_Psych_Nodes_Mapping target
+        when Psych::Nodes::Sequence then visit_Psych_Nodes_Sequence target
+        when Psych::Nodes::Alias    then visit_Psych_Nodes_Alias target
+        when Psych::Nodes::Document then visit_Psych_Nodes_Document target
+        when Psych::Nodes::Stream   then visit_Psych_Nodes_Stream target
         else
           raise "Can't handle #{target}"
         end
