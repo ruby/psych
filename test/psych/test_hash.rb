30e7b649c758235878ada7400f9c515fe628eea4
Julik <me@julik.nl>
2012-02-10 14:23:41 +0100

    Add a hash roundtrip test with UTF-8 key and value

--- test/psych/test_hash.rb
+++ test/psych/test_hash.rb
@@ -21,6 +21,15 @@ module Psych
       assert_equal X, x.class
     end
 
+    def test_hash_roundtrip_with_utf8_key_and_value
+      string = [1055, 1086, 1079, 1086, 1088, 1080, 1097, 1077].pack("U*")
+      string.force_encoding 'utf-8'
+      hash = {string => string}
+      yml = Psych.dump hash
+      assert_equal "---\n#{string}: #{string}\n", yml
+      assert_equal hash, Psych.load(yml)
+    end
+    
     def test_self_referential
       @hash['self'] = @hash
       assert_cycle(@hash)
