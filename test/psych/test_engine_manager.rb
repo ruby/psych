851de5c2310d4e2dd38e95b678be1f08e2fdb4da
Aaron Patterson <aaron.patterson@gmail.com>
2012-10-22 14:26:08 -0700

    merging from ruby trunk

--- test/psych/test_engine_manager.rb
+++ test/psych/test_engine_manager.rb
@@ -3,10 +3,6 @@ require 'yaml'
 
 module Psych
  class TestEngineManager < TestCase
-   def teardown
-     YAML::ENGINE.yamler = 'syck'
-   end
-
    def test_bad_engine
      assert_raises(ArgumentError) do
        YAML::ENGINE.yamler = 'foooo'
@@ -17,18 +13,6 @@ module Psych
      YAML::ENGINE.yamler = 'psych'
      assert_equal Psych, YAML
      assert_equal 'psych', YAML::ENGINE.yamler
-   end
-
-   def test_set_syck
-     YAML::ENGINE.yamler = 'syck'
-     assert_equal ::Syck, YAML
-     assert_equal 'syck', YAML::ENGINE.yamler
-   rescue NameError => e
-     if e.name == :Syck then
-       skip 'syck has been removed'
-     else
-       raise
-     end
    end
 
    A = Struct.new(:name)
