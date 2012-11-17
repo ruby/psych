3b066cf94a4f37e0395b2b761cda1862a13fcbd9
Aaron Patterson <aaron.patterson@gmail.com>
2012-11-17 12:49:31 -0800

    only embed libyaml if the system does not have it

--- ext/psych/extconf.rb
+++ ext/psych/extconf.rb
@@ -1,6 +1,9 @@
 require 'mkmf'
+require 'fileutils'
 
 # :stopdoc:
+
+dir_config 'libyaml'
 
 have_header 'dlfcn.h'
 have_header 'inttypes.h'
@@ -12,7 +15,20 @@ have_header 'string.h'
 have_header 'sys/stat.h'
 have_header 'sys/types.h'
 have_header 'unistd.h'
-have_header 'config.h'
+
+unless find_header('yaml.h') && find_library('yaml', 'yaml_get_version')
+  srcdir = File.expand_path File.dirname __FILE__
+  files = Dir.chdir File.join(srcdir, 'yaml') do
+    Dir.entries(Dir.pwd).find_all { |f|
+      File.file? f
+    }.map { |f| File.expand_path f }
+  end
+
+  FileUtils.cp_r files, srcdir
+
+  find_header 'yaml.h'
+  have_header 'config.h'
+end
 
 case RUBY_PLATFORM
 when /mswin/
