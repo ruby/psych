a38ea94c443e41d404d28926effe056ebc2c5750
Aaron Patterson <aaron.patterson@gmail.com>
2012-11-17 12:53:53 -0800

    only do have_header checks if we are embedding libyaml

--- ext/psych/extconf.rb
+++ ext/psych/extconf.rb
@@ -5,21 +5,8 @@ require 'fileutils'
 
 dir_config 'libyaml'
 
-have_header 'dlfcn.h'
-have_header 'inttypes.h'
-have_header 'memory.h'
-have_header 'stdint.h'
-have_header 'stdlib.h'
-have_header 'strings.h'
-have_header 'string.h'
-have_header 'sys/stat.h'
-have_header 'sys/types.h'
-have_header 'unistd.h'
-
 unless find_header('yaml.h') && find_library('yaml', 'yaml_get_version')
-  if $mswin
-    $CFLAGS += " -DYAML_DECLARE_STATIC -DHAVE_CONFIG_H"
-  end
+  # Embed libyaml since we could not find it.
 
   srcdir = File.expand_path File.dirname __FILE__
   files = Dir.chdir File.join(srcdir, 'yaml') do
@@ -29,6 +16,21 @@ unless find_header('yaml.h') && find_library('yaml', 'yaml_get_version')
   end
 
   FileUtils.cp_r files, srcdir
+
+  if $mswin
+    $CFLAGS += " -DYAML_DECLARE_STATIC -DHAVE_CONFIG_H"
+  end
+
+  have_header 'dlfcn.h'
+  have_header 'inttypes.h'
+  have_header 'memory.h'
+  have_header 'stdint.h'
+  have_header 'stdlib.h'
+  have_header 'strings.h'
+  have_header 'string.h'
+  have_header 'sys/stat.h'
+  have_header 'sys/types.h'
+  have_header 'unistd.h'
 
   find_header 'yaml.h'
   have_header 'config.h'
