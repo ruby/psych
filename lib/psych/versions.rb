e6a533eed532bbf9bf3fc01cf5fef216dd9114b6
Charles Oliver Nutter <headius@headius.com>
2017-02-24 09:14:22 -0600

    Bump version to 2.2.4.

--- lib/psych/versions.rb
+++ lib/psych/versions.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: false
 module Psych
   # The version is Psych you're using
-  VERSION = '2.2.3'
+  VERSION = '2.2.4'
 
   if RUBY_ENGINE == 'jruby'
     DEFAULT_SNAKEYAML_VERSION = '1.18'.freeze
