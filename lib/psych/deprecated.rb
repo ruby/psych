e2e867de11cec54ed508de2d6659be26b73cdb31
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-03-10 11:09:02 +0900

    Remove Psych.tagurize

--- lib/psych/deprecated.rb
+++ lib/psych/deprecated.rb
@@ -8,12 +8,6 @@ module Psych
     attr_accessor :taguri
     attr_accessor :to_yaml_style
   end
-
-  def self.tagurize thing
-    warn "#{caller[0]}: add_private_type is deprecated, use add_domain_type" if $VERBOSE
-    return thing unless String === thing
-    "tag:yaml.org,2002:#{thing}"
-  end
 end
 
 class Object
--- test/psych/test_deprecated.rb
+++ test/psych/test_deprecated.rb
@@ -121,11 +121,5 @@ module Psych
     def test_yaml_as
       assert_match(/helloworld/, Psych.dump(YamlAs.new))
     end
-
-    def test_tagurize
-      assert_nil Psych.tagurize nil
-      assert_equal Psych, Psych.tagurize(Psych)
-      assert_equal 'tag:yaml.org,2002:foo', Psych.tagurize('foo')
-    end
   end
 end
