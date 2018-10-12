1c747d89abddb8f3692ea7812c91534db6b5862d
Marcus Stollsteimer <sto.mar@web.de>
2018-10-12 16:18:02 +0200

    Fix typo

--- lib/psych/versions.rb
+++ lib/psych/versions.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Psych
-  # The version is Psych you're using
+  # The version of Psych you are using
   VERSION = '3.1.0.pre1' unless defined?(::Psych::VERSION)
 
   if RUBY_ENGINE == 'jruby'
